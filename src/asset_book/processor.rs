0cc9d7dca90361de9607abc8201cd2031970a422
porkytheblack <porkytheblack@gmail.com>
2025-11-27 04:14:50 +0300

    patch: lending pool updates

--- src/asset_book/processor.rs
+++ src/asset_book/processor.rs
@@ -1,98 +1,36 @@
-use anyhow::anyhow;
-use contract_integrator::utils::functions::asset_issuer::{AssetIssuerFunctionsInput, AssetIssuerFunctionsOutput, CreateAssetArgs};
-use contract_integrator::utils::functions::{ContractCallInput, ContractCallOutput};
-use contract_integrator::utils::functions::commons::get_contract_id_from_evm_address;
-use diesel::prelude::*;
-use diesel::{PgConnection, QueryDsl, RunQueryDsl};
-use diesel::r2d2::{ConnectionManager, PooledConnection};
-use uuid::Uuid;
 use crate::asset_book::config::AssetBookConfig;
 use crate::asset_book::db_types::{AssetBookRecord, AssetType, CreateAssetOnBook};
-use crate::asset_book::processor_enums::{AssetBookProcessorInput, AssetBookProcessorOutput, GetAssetInputArgs};
+use crate::asset_book::operations::create_asset;
+use crate::asset_book::processor_enums::{
+    AssetBookProcessorInput, AssetBookProcessorOutput, GetAssetInputArgs,
+};
 use crate::utils::app_config::AppConfig;
 use crate::utils::traits::ActionProcessor;
+use anyhow::anyhow;
+use contract_integrator::utils::functions::asset_issuer::{
+    AssetIssuerFunctionsInput, AssetIssuerFunctionsOutput, CreateAssetArgs,
+};
+use contract_integrator::utils::functions::commons::get_contract_id_from_evm_address;
+use contract_integrator::utils::functions::{ContractCallInput, ContractCallOutput};
+use diesel::prelude::*;
+use diesel::r2d2::{ConnectionManager, PooledConnection};
+use diesel::{PgConnection, QueryDsl, RunQueryDsl};
+use uuid::Uuid;
 
 impl ActionProcessor<AssetBookConfig, AssetBookProcessorOutput> for AssetBookProcessorInput {
-    async fn process(&self, app_config: &mut AppConfig, local_config: &mut AssetBookConfig, conn: Option<&mut PooledConnection<ConnectionManager<PgConnection>>>) -> anyhow::Result<AssetBookProcessorOutput> {
+    async fn process(
+        &self,
+        app_config: &mut AppConfig,
+        local_config: &mut AssetBookConfig,
+        conn: Option<&mut PooledConnection<ConnectionManager<PgConnection>>>,
+    ) -> anyhow::Result<AssetBookProcessorOutput> {
         let contract_ids = app_config.wallet.get_contract_ids()?;
-        let app_conn = conn.ok_or_else(||anyhow!("Unable to retrieve connection"))?;
+        let app_conn = conn.ok_or_else(|| anyhow!("Unable to retrieve connection"))?;
 
         match self {
             AssetBookProcessorInput::CreateNewAsset(args) => {
-
-                let result = match args.asset_type.clone() {
-                    AssetType::Bridged => {
-                        let input = ContractCallInput::BridgedAssetIssuer(
-                            AssetIssuerFunctionsInput::CreateAsset(CreateAssetArgs {
-                                contract_id: contract_ids.bridged_asset_issuer_contract_id.to_string(),
-                                symbol: args.symbol.clone(),
-                                name: args.name.clone(),
-                                acl_contract: contract_ids.access_controller_contract_id.to_solidity_address()?,
-                                allow_list:2
-                            })
-                        );
-
-                        let output =  app_config.wallet.execute(
-                            input
-                        ).await?;
-
-                        match output {
-                            ContractCallOutput::BridgedAssetIssuer(
-                                AssetIssuerFunctionsOutput::CreateAsset(res)
-                            ) => {
-                                res.output.ok_or_else(||anyhow!("Failed to retrieve result"))?
-                            },
-                            _=> return Err(anyhow!("Unable to find asset result"))
-                        }
-
-                    }
-                    AssetType::Native => {
-                        let input = ContractCallInput::NativeAssetIssuer(
-                            AssetIssuerFunctionsInput::CreateAsset(CreateAssetArgs {
-                                contract_id: contract_ids.native_asset_issuer_contract_id.to_string(),
-                                symbol: args.symbol.clone(),
-                                name: args.name.clone(),
-                                acl_contract: contract_ids.access_controller_contract_id.to_solidity_address()?,
-                                allow_list: 1
-                            })
-                        );
-
-                        let output =  app_config.wallet.execute(
-                            input
-                        ).await?;
-
-                        match output {
-                            ContractCallOutput::NativeAssetIssuer(
-                                AssetIssuerFunctionsOutput::CreateAsset(res)
-                            ) => {
-                                res.output.ok_or_else(||anyhow!("Failed to retrieve result"))?
-                            },
-                            _=>return Err(anyhow!("Failed to retrieve result"))
-                        }
-                    }
-                    _=>{
-                        unimplemented!("asset type not supported")
-                    }
-                };
-
-                let asset_manager_contract_id = get_contract_id_from_evm_address(&result.asset_manager).await?;
-
-                let input = CreateAssetOnBook {
-                    asset_manager: asset_manager_contract_id.to_string(),
-                    token: result.token,
-                    name: args.name.clone(),
-                    symbol: args.symbol.clone(),
-                    asset_type: Some(args.asset_type.clone()),
-                    decimals: args.decimals,
-                    icon: Some(args.icon.clone())
-                };
-
-                use crate::schema::asset_book::dsl::*;
-                use crate::schema::asset_book as AssetBookTable;
-
-
-                let asset_id = diesel::insert_into(AssetBookTable::table).values(&input).returning(id).get_result::<Uuid>(app_conn)?;
-
+                let mut wallet = app_config.wallet.clone();
+                let asset_id = create_asset(&mut wallet, app_conn, args.clone()).await?;
 
                 Ok(AssetBookProcessorOutput::CreateNewAsset(asset_id))
             }
@@ -104,47 +42,35 @@ impl ActionProcessor<AssetBookConfig, AssetBookProcessorOutput> for AssetBookPro
                     asset_type: Some(args.asset_type.clone()),
                     symbol: args.symbol.clone(),
                     name: args.name.clone(),
-                    token: args.token.clone()
+                    token: args.token.clone(),
                 };
 
-                use crate::schema::asset_book::dsl::*;
                 use crate::schema::asset_book as AssetBookTable;
+                use crate::schema::asset_book::dsl::*;
 
-
-                let asset_id = diesel::insert_into(AssetBookTable::table).values(&input).returning(id).get_result::<Uuid>(app_conn)?;
-
+                let asset_id = diesel::insert_into(AssetBookTable::table)
+                    .values(&input)
+                    .returning(id)
+                    .get_result::<Uuid>(app_conn)?;
 
                 Ok(AssetBookProcessorOutput::CreateExistingAsset(asset_id))
             }
             AssetBookProcessorInput::GetAsset(args) => {
-
                 use crate::schema::asset_book::dsl::*;
 
                 let mut query = asset_book.into_boxed();
 
                 query = match args {
-                    GetAssetInputArgs::ById(asset_id)=>{
-                        query.filter(
-                            id.eq(asset_id)
-                        )
-                    },
-                    GetAssetInputArgs::ByToken(token_value)=>{
-                        query.filter(
-                            token.eq(token_value)
-                        )
-                    },
-                    GetAssetInputArgs::ByAssetManager(manager_value)=>{
-                        query.filter(
-                            asset_manager.eq(manager_value)
-                        )
+                    GetAssetInputArgs::ById(asset_id) => query.filter(id.eq(asset_id)),
+                    GetAssetInputArgs::ByToken(token_value) => query.filter(token.eq(token_value)),
+                    GetAssetInputArgs::ByAssetManager(manager_value) => {
+                        query.filter(asset_manager.eq(manager_value))
                     }
                 };
 
                 let result = query.get_result::<AssetBookRecord>(app_conn)?;
 
-
                 Ok(AssetBookProcessorOutput::GetAsset(result))
-
             }
         }
     }
